4011029c3052ed66b484ef321596164ebacfdce3
ASO1801033 <1801033@s.asojuku.ac.jp>
2019-12-18 21:29:16 +0900

    練習02(算数ドリル)

--- pra_02.rb
+++ pra_02.rb
@@ -1,11 +1,57 @@
-puts "あなたの名前を教えてください！"
-name = gets
-puts "こんにちは！" + name
-print "さん"
+puts "算数ドリル"
+puts "スタートしますか？ はい＝1 / いいえ＝0"
+switch = gets.to_i
+cnt = 0
 
-puts "今" + name
-print "さんは何歳ですか？"
-age = gets.to_i
-puts age.to_s + "歳だと、私より年下ですねぇ"
-puts "私は100歳です！"
-puts "おわり"
+case switch
+when 0 then #thenは省略可能
+  puts "おわります"
+when 1 #whenは複数記載可能
+  puts "問題は3つあります！"
+  puts "第1問　1 + 3"
+  print "答え："
+  ans1 = gets.to_i
+  if ans1 == 1 + 3
+    puts "正解です！！"
+    cnt += 1
+    puts ""
+  else
+    puts "違います、、"
+    puts ""
+  end
+
+  puts "第2問　4 + 2"
+  print "答え："
+  ans2 = gets.to_i
+  if ans2 == 4 + 2
+    puts "正解です！！"
+    cnt += 1
+    puts ""
+  else
+    puts "違います、、"
+    puts ""
+  end
+
+  puts "第3問　5 + 7"
+  print "答え："
+  ans3 = gets.to_i
+  if ans3 == 5 + 7
+    puts "正解です！！"
+    cnt += 1
+    puts ""
+  else
+    puts "違います、、"
+    puts ""
+  end
+
+  puts "合計の正解数は…"
+  puts cnt.to_s + "問でした！"
+
+  if cnt == 3
+    puts "よくできました！"
+  elsif cnt == 0
+    puts "もっとがんばりましょう"
+  end
+else #上記以外の場合
+  puts "おわります"
+end
