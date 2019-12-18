5ff1477956d825a4ab643d7d27ceb4a6aca58225
ASO1801033 <1801033@s.asojuku.ac.jp>
2019-12-18 20:55:32 +0900

    練習02

--- pra_02.rb
+++ pra_02.rb
@@ -1,8 +1,11 @@
 puts "あなたの名前を教えてください！"
 name = gets
-puts "こんにちは！" + name + "さん"
+puts "こんにちは！" + name
+print "さん"
 
-puts "今" + name + "さんは何歳ですか？"
+puts "今" + name
+print "さんは何歳ですか？"
 age = gets.to_i
 puts age.to_s + "歳だと、私より年下ですねぇ"
 puts "私は100歳です！"
+puts "おわり"
