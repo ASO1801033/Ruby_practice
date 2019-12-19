da7d637cf6cbfbe7c510ad03c6895aa4de28127e
ASO1801033 <1801033@s.asojuku.ac.jp>
2019-12-19 12:37:18 +0900

    練習04(野菜当てゲーム)_配列宣言の練習

---
+++ pra_04.rb
@@ -0,0 +1,21 @@
+fruits = ["りんご", "いちご", "みかん", "きゅうり", "ぶどう"]
+no = 0
+
+while no <= fruits.length - 1
+  print "[" + no.to_s + "] "
+  puts fruits[no.to_i]
+  no += 1
+end
+
+no -= 1
+puts "このなかで野菜が入っているのは何番目？(0〜" + no.to_s + ")の値を入力しましょう！"
+print "答え："
+ans = gets.to_i
+
+if ans > no.to_i || ans < 0
+  puts "強制終了！！"
+elsif ans == 3
+  puts "正解です！！"
+else
+  puts "不正解です、、"
+end
