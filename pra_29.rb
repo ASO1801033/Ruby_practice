555e05ee78c608d2c707306fa6cd5b3c243f9194
ASO1801033 <1801033@s.asojuku.ac.jp>
2020-01-10 13:49:19 +0900

    練習29(うるう年判定キット)_strftime("%j")メソッドの練習

---
+++ pra_29.rb
@@ -0,0 +1,17 @@
+require "date" #DateとDateTimeクラスを使えるようにする
+
+puts "うるう年判定キット！"
+print "判定する西暦(1873年以降)を入力してください："
+year = gets.to_i
+
+if year < 1873
+  puts "入力エラーです、、"
+else
+  yeardays = Time.new(year, 12, 31)
+  if (yeardays.strftime("%j")).to_i == 366 #平年は355、うるう年は366が返される
+    #変数名.strftime("%j")で年中の通算日(001-366)を求める
+    puts "判定結果：うるう年です！"
+  else
+    puts "判定結果：平年です！"
+  end
+end
