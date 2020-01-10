4d7871b253cd9b0a9b35684214f5ea6377db23b6
Ayasan <57512994+ASO1801033@users.noreply.github.com>
2020-01-10 15:34:19 +0900

    練習29(うるう年判定キット)_strftime("%j")メソッドの練習

--- pra_29.rb
+++ pra_29.rb
@@ -5,7 +5,7 @@ print "判定する西暦(1873年以降)を入力してください："
 year = gets.to_i
 
 if year < 1873
-  puts "入力エラーです、、"
+  puts "1873よひ小さい値が入力されました、、"
 else
   yeardays = Time.new(year, 12, 31)
   if (yeardays.strftime("%j")).to_i == 366 #平年は355、うるう年は366が返される
